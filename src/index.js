826677df2e65703642414af499d2395e8b1c7887
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-06-24 19:20:06 +0100

    Updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import { createGameBoard } from "./modules/gameboard.js";
 
 createGameBoard();
 
-let knight = document.createElement("img");
-knight.src = "../src/assets/knight.jpg";
+function gameStart() {
+    let knight = document.createElement("img");
+    knight.src = "../src/assets/knight.jpg";
 
-document.getElementById("0,0").append(knight);
+    document.getElementById("0,0").append(knight);
+}
+
+gameStart();
