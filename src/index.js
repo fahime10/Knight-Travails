e63fac21ee7b6468f764bfc90cca3322e980353e
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-06-28 16:53:55 +0100

    Added functions to handle coordinates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 import { createGameBoard } from "./modules/gameboard.js";
 
 createGameBoard();
-
-function gameStart() {
-    let knight = document.createElement("img");
-    knight.src = "../src/assets/knight.jpg";
-
-    document.getElementById("0,0").append(knight);
-}
-
-gameStart();
